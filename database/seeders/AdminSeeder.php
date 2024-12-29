e6fc52d98626d03a91d33bed4ea31a4d4568d3a7
Saumy Anand <samcool3203@gmail.com>
2024-12-29 16:32:46 +0530

    database seeder added

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@dealindia.com',
+            'password' => Hash::make('dealindia123'),
+            'mobile' => '8555667788',
+            'dob' => '1990-01-01',
+            'address' => 'Admin Address',
+            'isAdmin' => '1',
+            'gender' => 'male',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
